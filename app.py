79d97c82bde2a5dae432276d660fd665ea81626d
Abhiram Dodda <94588496+AbhiramDodda@users.noreply.github.com>
2023-11-28 15:35:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template, request
+from Text_Summarization_Information_Extraction import *
 
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+  data = ""
+  if method == "POST":
+    pass
+
+  return render_template("index.html", data = data)
+    
+
+
+if __name__ == "__main__":
+  app.run(debug = True)
