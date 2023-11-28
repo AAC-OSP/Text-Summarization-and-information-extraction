214a956703650361780d857fade0934a3b10f1bc
Abhiram Dodda <94588496+AbhiramDodda@users.noreply.github.com>
2023-11-28 16:03:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template, request
 from Text_Summarization_Information_Extraction import *
+import os
 
 app = Flask(__name__)
 
+#Controller
 @app.route("/", methods = ["GET", "POST"])
 def index():
   data = ""
   if method == "POST":
-    pass
+    if 'file' in request.files:
+      file = request.files['file']
+      file_type = os.path.split(file)[1]
+      text = ''
+      if file_type == 'pdf':
+        text = extract_text_pdf(file)
+      elif file_type == 'txt':
+        with open(file, 'r') as f:
+          text = f.read()
+      result = summarize_text(text)
+      data = result
+    else:
+      text = request.get('text')
+      result = summarize_text(text)
+      data = result
 
   return render_template("index.html", data = data)
     
